aba3702165d3b7dddfdfd71fb42fe19d2a964647
KGr1sha <krylov.grigory@gmail.com>
2025-06-07 18:37:35 +0300

    remade led matrix

--- Proteus/src/main.c
+++ Proteus/src/main.c
@@ -2,24 +2,38 @@
 #include <stdint.h>
 
 #define uint8 uint8_t
-#define blink_delay 10000
+#define delay for(int i = 0; i < 30000; i++)
 
-#define R1_PIN P1_0
-#define R2_PIN P1_1
-#define R3_PIN P1_2
-#define C1_PIN P1_3
-#define C2_PIN P1_4
-#define C3_PIN P1_5
-#define X_PIN  P1_6
-#define O_PIN  P1_7
+#define X1 P1_0
+#define X2 P1_1
+#define X3 P1_2
+#define X4 P1_3
+#define X5 P1_4
+#define X6 P1_5
+#define X7 P1_6
+#define X8 P1_7
+#define X9 P0_2
+#define O1 P0_3
+#define O2 P0_4
+#define O3 P0_5
+#define O4 P0_6
+#define O5 P0_7
+#define O6 P2_5
+#define O7 P2_6
+#define O8 P2_7
+#define O9 P3_2
+
+#define A P2_0
+#define B P2_1
+#define C P2_2
+#define D P2_3
 
 #define CURSOR_PIN P0_0
 #define SELECT_PIN P0_1
-#define TURN_PIN P0_2 // should go low to light up led
+#define TURN_PIN P2_4 // should go low to light up led
 
-uint8 board[9]; // 0 - nothing; 1 - X; 2 - O;
+
 uint8 cursor; // 1 - X, 2 - O;
-uint8 player;
 
 // initializing com port
 void UART_init(void){ 
@@ -48,117 +62,106 @@ void UART_write(uint8 data){
 	TI = 0;
 }
 
-void reset_board(void) {
-	for (int i = 0; i < 9; i++) {
-		board[i] = 0;
-	}
-}
-
-void reset_board_visual(void) {
-	P1 = 0b00111000; // 0,1,2 - rows; 3,4,5 - columns; 6,7 - X and O;
+void reset_led_matrix(void) {
+	X1 = 1;
+	X2 = 1;
+	X3 = 1;
+	X4 = 1;
+	X5 = 1;
+	X6 = 1;
+	X7 = 1;
+	X8 = 1;
+	X9 = 1;
+	O1 = 1;
+	O2 = 1;
+	O3 = 1;
+	O4 = 1;
+	O5 = 1;
+	O6 = 1;
+	O7 = 1;
+	O8 = 1;
+	O9 = 1;
 }
 
 void reset_num_display(void) {
-	P2 = 0b01111111;
+	A = 0;
+	B = 0;
+	C = 0;
+	D = 0;
 }
 
 void display_digit(int digit) {
-	//    a
-	//  |---|
-	// f| g | b
-	//  |---|
-	// e|   | c
-	//  |---|
-	//   d
-	switch(digit) {
-		case 0: {
-			P2 = 0b01000000;
-		}
-		case 1: {
-			P2 = 0b01111001;
-		} break;
-		case 2: {
-			P2 = 0b00100100;
-		} break;
-		case 3: {
-			P2 = 0b00110000;
-		} break;
-		case 4: {
-			P2 = 0b00011001;
-		} break;
-		case 5: {
-			P2 = 0b00010010;
-		} break;
-		case 6: {
-			P2 = 0b00000010;
-		} break;
-		case 7: {
-			P2 = 0b01111000;
-		} break;
-		case 8: {
-			P2 = 0b00000000;
-		} break;
-		case 9: {
-			P2 = 0b00010000;
-		} break;
-		default: {
-			P2 = 0b011111111;		 
-		}
-	}
+	A = digit & (1 << 0) ? 1 : 0;
+	B = digit & (1 << 1) ? 1 : 0;
+	C = digit & (1 << 2) ? 1 : 0;
+	D = digit & (1 << 3) ? 1 : 0;
 }
 
-// we can display the board with just 8 pins.
-// 3 pins - rows
-// 3 pins - columns
-// 2 pins - X or O
-// need 2 transistors for each row
-void scan_display(void) {
-	for (uint8 r = 0; r < 3; r++) {
-		reset_board_visual();
-        
-        // Activate only row “r”
-        if (r == 0) {
-		   	R1_PIN = 1;
+void display_shape(int cell, int shape) {
+	if (shape == 1) { 
+	// X
+		switch(cell) {
+			case 0:
+				X1 = 0;
+				break;
+			case 1:
+				X2 = 0;
+				break;
+			case 2:
+				X3 = 0;
+				break;
+			case 3:
+				X4 = 0;
+				break;
+			case 4:
+				X5 = 0;
+				break;
+			case 5:
+				X6 = 0;
+				break;
+			case 6:
+				X7 = 0;
+				break;
+			case 7:
+				X8 = 0;
+				break;
+			case 8:
+				X9 = 0;
+				break;
 		}
-        else if (r == 1) {
-		   	R2_PIN = 1; 
+	}
+	if (shape == 2) { 
+	// O
+		switch(cell) {
+			case 0:
+				O1 = 0;
+				break;
+			case 1:
+				O2 = 0;
+				break;
+			case 2:
+				O3 = 0;
+				break;
+			case 3:
+				O4 = 0;
+				break;
+			case 4:
+				O5 = 0;
+				break;
+			case 5:
+				O6 = 0;
+				break;
+			case 6:
+				O7 = 0;
+				break;
+			case 7:
+				O8 = 0;
+				break;
+			case 8:
+				O9 = 0;
+				break;
 		}
-        else {
-		   	R3_PIN = 1; 
-		}
-        
-        // to light led we do (row=1, col=0).
-
-		// drawing X's
-		X_PIN = 1;
-		O_PIN = 0;
-        for (uint8 c = 0; c < 3; c++) {
-            uint8 idx = r*3 + c;
-            if (board[idx] == 1) {
-				if      (c == 0) C1_PIN = 0;
-				else if (c == 1) C2_PIN = 0;
-				else if (c == 2) C3_PIN = 0;
-			}
-        }
-		// delay
-        for (uint16_t d = 0; d < blink_delay; d++);
-		// drawing O's
-		X_PIN = 0;
-		O_PIN = 1;
-		C1_PIN = 1;
-		C2_PIN = 1;
-		C3_PIN = 1;
-        for (uint8 c = 0; c < 3; c++) {
-            uint8 idx = r*3 + c;
-            if (board[idx] == 2) {
-				if      (c == 0) C1_PIN = 0;
-				else if (c == 1) C2_PIN = 0;
-				else if (c == 2) C3_PIN = 0;
-			}
-        }
-		// delay
-        for (uint16_t d = 0; d < blink_delay; d++);
-    }
+	}
 }
 
 void process_data(void) {
@@ -171,18 +174,20 @@ void process_data(void) {
 		// s - placed shape: 0 - X, 1 - O
 		// d - if r: 0; if c or s: cell
 		if (data & (1 << 7)) {
-			reset_board();
-			reset_board_visual();
+			// reset
+			reset_led_matrix();
 			cursor = 0;
 			display_digit(1);
 			TURN_PIN = 1;
 		} else if (data & (1 << 6)) {
+			// cursor placement
 			cursor = data & 0x0F;
 			display_digit(cursor + 1);
 		} else {
+			// shape placement
 			int shape = (data & (1 << 4)) == 0? 1 : 2; // maps 0,1 -> 1,2
 			int cell = data & 0x0F; // just take 4 least bits
-			board[cell] = shape;
+			display_shape(cell, shape);
 			cursor = 0;
 			display_digit(1);
 			TURN_PIN = shape - 1;
@@ -204,21 +209,20 @@ void process_data(void) {
 		data |= (1 << 7);
 		data |= cursor;
 		UART_write(data);
+		delay;
 	} else if (SELECT_PIN == 0) {
 		uint8 data = 0 | (1 << 6);
 		UART_write(data);
+		delay;
 	}
 }
 
 void main(void) {
 	UART_init();
-	reset_board();
+	reset_led_matrix();
 	reset_num_display();
-	reset_board_visual();
-	display_digit(1);
-	TURN_PIN = 1;
 	while(1) {
 		process_data();
-		scan_display();
 	}
 }
+
