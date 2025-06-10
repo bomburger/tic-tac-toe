0b79da10d5b6e7f971657e93ef2b4f22d049e840
grisha <krylov.grigory@gmail.com>
2025-06-10 17:55:37 +0300

    got 10

--- Proteus/src/main.c
+++ Proteus/src/main.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 
 #define uint8 uint8_t
-#define delay for(int i = 0; i < 30000; i++)
 
 #define X1 P1_0
 #define X2 P1_1
@@ -21,28 +20,36 @@
 #define O6 P2_5
 #define O7 P2_6
 #define O8 P2_7
-#define O9 P3_2
+#define O9 P0_1
 
 #define A P2_0
 #define B P2_1
 #define C P2_2
 #define D P2_3
 
-#define CURSOR_PIN P0_0
-#define SELECT_PIN P0_1
 #define TURN_PIN P2_4 // should go low to light up led
-
 
 static uint8 cursor; // 1 - X, 2 - O;
 static uint8 board[9];
+static uint8 byte_to_send;
+static uint8 is_byte_to_send;
 
 // initializing com port
-void UART_init(void){ 
+void UART_init(void) { 
 	TMOD = 0x20;
 	SCON = 0x50;
 	TH1 = 0xFD;
 	TL1 = 0xFD;
 	TR1 = 1;
+}
+
+void configure_interrupts(void) {
+	IT0 = 1;
+	IT1 = 1;
+	EX0 = 1;
+	EX1 = 1;
+	ES = 1;
+	EA = 1;
 }
 
 // shows if there is data in com port
@@ -56,11 +63,10 @@ uint8 UART_read(void){
 	return SBUF;
 }
 
-// send 1 byte
-void UART_write(uint8 data){
-	SBUF = data;
-	while (TI != 1);
-	TI = 0;
+void add_byte_to_write_queue(uint8 byte) {
+	if (is_byte_to_send) { return; }
+	byte_to_send = byte;
+	is_byte_to_send = 1;
 }
 
 void reset_led_matrix(void) {
@@ -206,22 +212,30 @@ void display_shape(int cell, int shape) {
 	}
 }
 
-void process_data(void) {
-	if (UART_is_not_empty()) {
-		uint8 data = UART_read();
-		// FORMAT:
-		// rc0sdddd
-		// r - reset the board
-		// c - cursor
-		// s - placed shape: 0 - X, 1 - O
-		// d - if r: 0; if c or s: cell
+void reset(void) {
+	for (int i = 0; i < 9; i++) { board[i] = 0; }
+	cursor = 0;
+	reset_led_matrix();
+	reset_num_display();
+	display_digit(1);
+	display_shape(0, 1);
+	display_shape(0, 2);
+	TURN_PIN = 1;
+}
+
+void serial_interrupt(void) __interrupt(4) {
+	// FORMAT:
+	// rcxsdddd
+	// r - reset the board
+	// c - cursor
+	// s - placed shape: 0 - X, 1 - O
+	// d - if r: 0; if c or s: cell
+	if (RI) {
+		RI = 0;
+		uint8 data = SBUF;
+
 		if (data & (1 << 7)) {
-			// reset
-			reset_led_matrix();
-			cursor = 0;
-			display_digit(1);
-			TURN_PIN = 1;
-			for (int i = 0; i < 9; i++) board[i] = 0;
+			reset();
 		} else if (data & (1 << 6)) {
 			// cursor placement
 			display_shape(cursor, 0);
@@ -237,53 +251,50 @@ void process_data(void) {
 				
 			display_shape(cell, shape);
 
-			//cursor = 0;
-			//display_digit(1);
-
 			TURN_PIN = shape - 1;
 			board[cell] = shape;
 		}
 	}
+}
 
-	// FORMAT:
-	// ccccdddd
-	// c - code
-	// d - data
-	// for cursor:
-	// cccc <- 1000; dddd <- cursor
-	// for select:
-	// cccc <- 0100; dddd <- 0000
-	if (CURSOR_PIN == 0) {
-		display_shape(cursor, 0);
-		display_shape(cursor, board[cursor]);
-		cursor = (cursor + 1) % 9;
-		display_shape(cursor, 1);
-		display_shape(cursor, 2);
-		display_digit(cursor + 1);
-		uint8 data = 0;
-		data |= (1 << 7);
-		data |= cursor;
-		UART_write(data);
-		delay;
-	} else if (SELECT_PIN == 0) {
-		uint8 data = 0 | (1 << 6);
-		UART_write(data);
-		delay;
-	}
+// ccccdddd
+// for cursor:
+// cccc <- 1000; dddd <- cursor
+void int0_interrupt(void) __interrupt(0) {
+	// Move cursor
+	display_shape(cursor, 0);
+	display_shape(cursor, board[cursor]);
+	cursor = (cursor + 1) % 9;
+	display_shape(cursor, 1);
+	display_shape(cursor, 2);
+	display_digit(cursor + 1);
+	uint8 data = 0;
+	data |= (1 << 7);
+	data |= cursor;
+	add_byte_to_write_queue(data);
+}
+
+// ccccdddd
+// for select:
+// cccc <- 0100; dddd <- 0000
+void int1_interrupt(void) __interrupt(2) {
+	// Place shape
+	uint8 data = 0 | (1 << 6);
+	add_byte_to_write_queue(data);
 }
 
 void main(void) {
 	UART_init();
-	reset_led_matrix();
-	reset_num_display();
-	for (int i = 0; i < 9; i++) {
-		board[i] = 0;
-	}
-	display_digit(1);
-	display_shape(0, 1);
-	display_shape(0, 2);
+	reset();
+	configure_interrupts();
 	while(1) {
-		process_data();
+		if (is_byte_to_send == 1) {
+			SBUF = byte_to_send;
+			TI = 1;
+			while (TI != 1);
+			TI = 0;
+			is_byte_to_send = 0;
+		}
 	}
 }
 
